afa975e00f7c8c1ed69425ae867a8a196fc89f8e
wnsgk <unwnsgk1282@naver.com>
2023-04-12 23:19:58 +0900

    test update

--- src/test/java/com/book/service/FollowServiceTest.java
+++ src/test/java/com/book/service/FollowServiceTest.java
@@ -5,13 +5,17 @@ import com.book.domain.user.User;
 import com.book.domain.user.UserCreateDto;
 import com.book.exception.user.DuplicateEmailException;
 import com.book.repository.FollowRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 @Transactional
@@ -24,15 +28,21 @@ class FollowServiceTest {
     @Autowired
     private FollowRepository followRepository;
 
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    public void init() {
+        UserCreateDto createDto1 = new UserCreateDto("kim2", "kim2@kim.com", "1234");
+        user1 = userService.signUp(createDto1);
+
+        UserCreateDto createDto2 = new UserCreateDto("kim3", "kim3@kim.com", "1234");
+        user2 = userService.signUp(createDto2);
+    }
+
     @Test
     @DisplayName("팔로우")
     void follow() {
-        UserCreateDto createDto1 = new UserCreateDto("kim2", "kim2@kim.com", "1234");
-        User user1 = userService.signUp(createDto1);
-
-        UserCreateDto createDto2 = new UserCreateDto("kim3", "kim3@kim.com", "1234");
-        User user2 = userService.signUp(createDto2);
-
         Follow follow = followService.follow(user2, user1);
         assertEquals(user1.getFollower().contains(follow), true);
         assertEquals(user2.getFollowing().contains(follow), true);
@@ -41,11 +51,6 @@ class FollowServiceTest {
     @Test
     @DisplayName("팔로우 중복")
     void duplicateFollow() {
-        UserCreateDto createDto1 = new UserCreateDto("kim2", "kim2@kim.com", "1234");
-        User user1 = userService.signUp(createDto1);
-
-        UserCreateDto createDto2 = new UserCreateDto("kim3", "kim3@kim.com", "1234");
-        User user2 = userService.signUp(createDto2);
 
         followService.follow(user1, user2);
         assertThrows(RuntimeException.class, () -> followService.follow(user1, user2));
@@ -54,11 +59,6 @@ class FollowServiceTest {
     @Test
     @DisplayName("팔로우 취소")
     void delete() {
-        UserCreateDto createDto1 = new UserCreateDto("kim2", "kim2@kim.com", "1234");
-        User user1 = userService.signUp(createDto1);
-
-        UserCreateDto createDto2 = new UserCreateDto("kim3", "kim3@kim.com", "1234");
-        User user2 = userService.signUp(createDto2);
 
         Follow follow = followService.follow(user1, user2);
         followService.followCancel(user1, user2);
---
+++ src/test/java/com/book/service/MyBookServiceTest.java
@@ -0,0 +1,104 @@
+package com.book.service;
+
+import com.book.domain.MyBook.MyBook;
+import com.book.domain.MyBook.MyBookCreateDto;
+import com.book.domain.user.User;
+import com.book.domain.user.UserCreateDto;
+import com.book.exception.book.BookNotFoundException;
+import com.book.exception.book.DuplicateBookException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@Transactional
+class MyBookServiceTest {
+
+    @Autowired private UserService userService;
+    @Autowired private MyBookService myBookService;
+
+    private User user;
+    private MyBookCreateDto createDto;
+    @BeforeEach
+    public void init() {
+        UserCreateDto createDto1 = new UserCreateDto("test", "test@kim.com", "1234");
+        user = userService.signUp(createDto1);
+        List<String> tags = new ArrayList<>();
+        tags.add("tag1");
+        tags.add("tag2");
+        createDto = MyBookCreateDto.builder()
+                .author("test")
+                .description("test")
+                .page(1)
+                .star(1)
+                .isbn("1234512345123")
+                .url("123")
+                .tags(tags)
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now())
+                .build();
+    }
+
+    @Test
+    @DisplayName("책 생성")
+    void create() {
+        MyBook myBook = myBookService.createMyBook(user, createDto);
+        assertEquals(user.getMyBooks().isEmpty(), false);
+        assertEquals(myBook.getIsbn(), "1234512345123");
+    }
+
+    @Test
+    @DisplayName("책 중복 생성")
+    void duplicateCreate(){
+        MyBook myBook = myBookService.createMyBook(user, createDto);
+        List<String> tags = new ArrayList<>();
+        tags.add("tag1");
+        tags.add("tag2");
+        MyBookCreateDto createDto1 = MyBookCreateDto.builder()
+                .author("test")
+                .description("test")
+                .page(1)
+                .star(1)
+                .isbn("1234512345123")
+                .url("123")
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now())
+                .tags(tags)
+                .build();
+        assertThrows(DuplicateBookException.class, () -> myBookService.createMyBook(user, createDto1));
+    }
+
+    @Test
+    @DisplayName("책 삭제")
+    void delete() {
+        MyBook myBook = myBookService.createMyBook(user, createDto);
+        myBookService.deleteMyBooks(myBook.getId());
+        assertThrows(BookNotFoundException.class, () -> myBookService.getMyBook(myBook.getId()));
+    }
+
+    @Test
+    @DisplayName("책 찾기")
+    void findBook(){
+        MyBook myBook = myBookService.createMyBook(user, createDto);
+        MyBook myBook1 = myBookService.getMyBook(myBook.getId());
+        assertEquals(myBook1, myBook);
+    }
+
+    @Test
+    @DisplayName("태그 확인")
+    void tag(){
+        MyBook myBook = myBookService.createMyBook(user, createDto);
+        assertEquals(myBook.getTags().size(), 2);
+    }
+}
